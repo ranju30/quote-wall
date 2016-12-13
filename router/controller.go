9f1b1a8e219c94021f52cda639f34b474ad6ee96
Ranju <ranjukr@thoughtworks.com>
2016-12-13 10:53:32 +0530

    @ranjukr: implemented controller for handle each request.

--- router/controller.go
+++ router/controller.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	quoteHandler "quote_wall/handler"
+)
+
+func HandleRequest() {
+	handler := mux.NewRouter();
+
+	handler.HandleFunc("/store", quoteHandler.SaveQuote()).Methods("POST")
+	//handler.HandleFunc("/store", quoteHandler.SaveQuote()).Methods("GET")
+	handler.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")));
+
+	http.Handle("/", handler);
+}
