c9a825c8a4f460e5344998da9da3da2f5a71c3db
Ranju <ranjukr@thoughtworks.com>
2016-12-13 10:52:51 +0530

    @ranjukr: implemented loadWall to serve the page after save and call handler according to the action.

--- public/js/loadWall.js
+++ public/js/loadWall.js
@@ -1,1 +1,23 @@
+function emptyField() {
+    $("#name").val("");
+    $("#textArea").val("");
+}
+function store() {
+    var toSaveName = $("#name").val();
+    var toSaveQuote = $("#textArea").val();
+    if (toSaveName != "" && toSaveQuote != "") {
+        emptyField();
+        $.post("/store", {name: toSaveName, quote: toSaveQuote}, function (res, err) {
+            $('#updateWall').append('<b>' + toSaveName + '</b>' + ' : ' + toSaveQuote + '<br>');
+        })
+    }
+}
+
+function allQuotes() {
+    $.get("/store", function (res, err) {
+        $('#output').html(res);
+    })
+}
+// window.load = allQuotes();
+
 
