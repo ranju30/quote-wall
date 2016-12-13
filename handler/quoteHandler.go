5d4ed3e66ec8a154ce248ac953b9d9bb76339256
Ranju <ranjukr@thoughtworks.com>
2016-12-13 10:50:16 +0530

    @ranjukr: implemented handler for output the task.

--- handler/quoteHandler.go
+++ handler/quoteHandler.go
@@ -1,1 +1,36 @@
 package handler
+
+import (
+	"strings"
+	"quote_wall/model"
+	"quote_wall/database"
+	"log"
+	"net/http"
+	"database/sql"
+)
+
+func openDB() *sql.DB {
+	db, err := database.OpenDatabase()
+	if (err != nil) {
+		log.Fatal(err.Error())
+	}
+	db.Ping();
+	return db;
+}
+
+func SaveQuote() http.HandlerFunc {
+	db := openDB();
+	return func(res http.ResponseWriter, req *http.Request) {
+		req.ParseForm();
+		name := strings.Join(req.Form["name"], "");
+		quote := strings.Join(req.Form["quote"], "");
+		quote_to_db := model.Quote{Quote:quote,Name:name}
+		err := database.StoreQuote(db, &quote_to_db)
+		if (err != nil) {
+			log.Fatal(err.Error())
+			res.Write([]byte("got error.."))
+			return
+		}
+		res.Write([]byte("Quote has stored.."));
+	}
+}
